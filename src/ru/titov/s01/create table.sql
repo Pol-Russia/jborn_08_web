d624e1695a8e0598bdb23b46e0414cadb76aab8e
Pawel Titov <polpolpol1974@yandex.ru>
2018-12-03 02:14:44 +0300

    correction commit

--- src/ru/titov/s01/create table.sql
+++ src/ru/titov/s01/create table.sql
@@ -1,4 +1,4 @@
-create table user_list(
+create table person(
                         id serial primary key not null,
                         full_name varchar(60) not null,
                         e_mail varchar(120),
@@ -7,8 +7,10 @@ create table user_list(
 
 create table account (
                        id serial primary key not null,
-                       user_list_id int not null references user(id),
-                       balance money not null
+                       person_id int not null references person(id),
+                       balance money not null,
+                       currency_id int not null references currency(id),
+                       description_id int not null references account_description(id)
 );
 
 create table transaction (
@@ -23,3 +25,25 @@ create table categorie (
                          id serial primary key not null,
                          description varchar(25) not null
 );
+
+create table currency (
+                       id serial primary key not null,
+                       name_of_currency varchar(20) not null
+);
+
+create table account_description (
+                      id serial primary key not null,
+                      description varchar(50) not null
+);
+
+                      create index name_index
+                      on person (full_name);
+
+                      create index sum
+                      on transaction (sum);
+
+alter table account
+                       add person_id int not null references person(id),
+                       add currency_id int not null references currency(id),
+                       add description_id int not null references account_description(id),
+                       drop  user_list_id;
---
+++ src/ru/titov/s01/select.sql
@@ -0,0 +1,25 @@
+4.a
+select p.full_name as name, account.id, account.balance  from person as p, account
+where p.id = account.person_id
+order by p.full_name;
+
+select p.full_name as name, account.id, account.balance  from person as p inner join account
+on p.id = account.person_id
+order by p.full_name;
+
+4.b
+select p.full_name as name, a.id as account, t.id, t.sum, t.date  from transaction as t
+inner join account a on (t.account_id = a.id and t.date = (now()::date - '1 day'::interval)::date)
+inner join person p on a.user_list_id = p.id
+order by p.full_name;
+
+--(now()::date - '1 day'::interval)::date) Получение вчерашней даты в postgres
+
+4.c
+select  p.full_name as name, sum(account.balance) from person as p, account
+where p.id = account.person_id
+group by p.full_name;
+
+select  p.full_name as name, sum(account.balance) from person as p inner join account
+on p.id = account.person_id
+group by p.id;
